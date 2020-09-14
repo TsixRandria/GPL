c873c1ae5c3d66fd6a58dc6c06f06cc0ee79f3e4
TsixRandria <randriamamonjisoanantenaina@gmail.com>
2020-09-14 11:03:43 +0300

    index_conditions_mentions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get '/index' => 'homepage#index', :as => 'index'
+  get '/conditions-generales-de-location' => 'homepage#conditions', :as => 'conditions'
+  get '/mentions-legales' => 'homepage#mentions', :as => 'mentions'
+
+  # get '/location-voiture-aeroport-reunion' => 
+  # get '/location-voiture-sainte-marie' => 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
