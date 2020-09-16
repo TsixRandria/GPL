39a48fbbd32cc11f0daa6615124a1b33c8deac21
TsixRandria <randriamamonjisoanantenaina@gmail.com>
2020-09-16 17:15:06 +0300

    controllercontactOK

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
 
+  
 root to:'homepage#index'
   get '/conditions-generales-de-location' => 'homepage#conditions', :as => 'conditions'
   get '/mentions-legales' => 'homepage#mentions', :as => 'mentions'
+
+  #contact
+  get '/contact' => 'contact#contact', :as => 'contact'
 
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :paiements
