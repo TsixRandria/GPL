0611da66d824136f1912ac5886142671dd93d005
jean-claude07 <jeclaude78@gmail.com>
2020-09-17 16:39:35 +0300

    client-branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+   devise_for :clients
+  
 root to:'homepage#index'
   get '/conditions-generales-de-location' => 'homepage#conditions', :as => 'conditions'
   get '/mentions-legales' => 'homepage#mentions', :as => 'mentions'
 
+  
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
