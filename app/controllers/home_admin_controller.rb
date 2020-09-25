a6948845b41628b0cb0d7c87d0f24881bccf140b
TsixRandria <randriamamonjisoanantenaina@gmail.com>
2020-09-25 14:16:07 +0300

    dev Ok

--- app/controllers/home_admin_controller.rb
+++ app/controllers/home_admin_controller.rb
@@ -1,8 +1,81 @@
 class HomeAdminController < ApplicationController
 	before_action :authenticate_admin!
-  def index
-  end
-
+ 
   def dashboard
   end
+
+
+
+   before_action :set_voiture, only: [:show, :edit, :update, :destroy]
+
+  # GET /voitures
+  # GET /voitures.json
+  def index
+    @voitures = Voiture.all
+  end
+
+  # GET /voitures/1
+  # GET /voitures/1.json
+  def show
+  end
+
+  # GET /voitures/new
+  def new
+    @voiture = Voiture.new
+  end
+
+  # GET /voitures/1/edit
+  def edit
+  end
+
+  # POST /voitures
+  # POST /voitures.json
+  def create
+    @voiture = Voiture.new(voiture_params)
+
+    respond_to do |format|
+      if @voiture.save
+        format.html { redirect_to homepage_admin_path, notice: 'Voiture was successfully created.' }
+        format.json { render :show, status: :created, location: @voiture }
+      else
+        format.html { render :new }
+        format.json { render json: @voiture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /voitures/1
+  # PATCH/PUT /voitures/1.json
+  def update
+    respond_to do |format|
+      if @voiture.update(voiture_params)
+        format.html { redirect_to homepage_admin_path, notice: 'Voiture was successfully updated.' }
+        format.json { render :show, status: :ok, location: @voiture }
+      else
+        format.html { render :edit }
+        format.json { render json: @voiture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /voitures/1
+  # DELETE /voitures/1.json
+  def destroy
+    @voiture.destroy
+    respond_to do |format|
+      format.html { redirect_to voitures_url, notice: 'Voiture was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_voiture
+      @voiture = Voiture.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def voiture_params
+      params.require(:voiture).permit(:image, :marque, :place, :vitesse, :types, :climatiseur, :status)
+    end
 end
