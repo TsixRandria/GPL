0f544547ae71de056a2b8dfa8c559830f1d006d3
jean-claude07 <jeclaude78@gmail.com>
2020-09-30 08:38:58 +0300

    client

--- app/controllers/clientsession_controller.rb
+++ app/controllers/clientsession_controller.rb
@@ -1,4 +1,23 @@
 class ClientsessionController < ApplicationController
-  def index
+before_action :configure_devise_parameters, if: :devise_controller?
+
+  # def index
+  	
+
+  # end
+
+ 
+
+
+  def update
+    respond_to do |format|
+      if @assurance.update(assurance_params)
+        format.html { redirect_to @assurance, notice: 'Assurance was successfully updated.' }
+        format.json { render :show, status: :ok, location: @assurance }
+      else
+        format.html { render :edit }
+        format.json { render json: @assurance.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
